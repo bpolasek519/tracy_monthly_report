1bc0fc4379a0f07b91c638ef088202df3e7a910d
Brittanie <brittanie.polasek@yahoo.com>
2023-11-30 22:24:42 -0600

    edited error message for filters

--- src/exceptions.py
+++ src/exceptions.py
@@ -9,5 +9,4 @@ def read_excel_with_exception(filename, sheet_name):
         return df
     except Exception as e:
         file = os.path.basename(filename)
-        raise Exception(f'Unable to read {file}. Please check your file and make adjustments as needed. '
-                        f'(Most common error is #DIV/0! error.)')
+        raise Exception(f'Unable to read {file}. Check that filters are turned off and there are no #DIV/0!')
